ad7ff828535e0febf8d607defd74037da4fa0318
itrummer <immanuel.trummer@gmail.com>
2024-03-05 11:22:49 -0500

    Created PostgreSQL environment, still debugging.

--- src/tupac/engine.py
+++ src/tupac/engine.py
@@ -3,8 +3,14 @@ Created on Mar 5, 2024
 
 @author: immanueltrummer
 '''
+import psycopg2
+import time
+
 from gymnasium import Env
 from gymnasium.spaces import Discrete
+
+nr_batches = 10
+""" Number of data batches that can be indexed. """
 
 
 class PgSimEnv(Env):
@@ -14,10 +20,11 @@ class PgSimEnv(Env):
         """ Initializes database for given credentials. """
         super().__init__()
         self.action_space = Discrete(3)
-        self.observation_space = Discrete(11)
+        self.observation_space = Discrete(nr_batches+1)
         self.nr_indexed = 0
         self.nr_steps = 0
         self.log = []
+        self.start_s = time.time()
         print('Init')
     
     def close(self):
@@ -51,12 +58,13 @@ class PgSimEnv(Env):
             print(f'Index one less batch ({self.nr_indexed} indexed)')
         
         reward = self._reward()
-        self.log += [(self.nr_steps, self.nr_indexed)]
+        total_s = time.time() - self.start_s
+        self.log += [(self.nr_steps, self.nr_indexed, total_s, reward)]
         return reward, self.nr_indexed, False, False, {}
     
     def _add_index(self):
         """ Simulates adding one index. """
-        self.nr_indexed = min(10, self.nr_indexed+1)
+        self.nr_indexed = min(nr_batches, self.nr_indexed+1)
         
     def _drop_index(self):
         """ Simulates dropping one index. """
@@ -68,4 +76,91 @@ class PgSimEnv(Env):
         Returns:
             Calculated reward value.
         """
-        return self.nr_indexed / 10.0
+        return self.nr_indexed / nr_batches
+
+
+class PgEnv(PgSimEnv):
+    """ Tuning environment for Postgres. """
+    
+    def __init__(self, db, user, password):
+        """ Initializes connection to database.
+        
+        Args:
+            db: name of database to tune.
+            user: name of database user.
+            password: password for database access.
+        """
+        super().__init__()
+        self.connection = psycopg2.connect(
+            f'dbname={db} user={user} password={password}')
+        self.reset(0)
+        self.default_secs = self._benchmark()
+    
+    def reset(self, seed):
+        """ Reset database state by dropping indexes.
+        
+        Args:
+            seed: seed value (not used).
+        """
+        for i in range(nr_batches):
+            sql = f'drop index if exists shipdateindex{i};'
+            self._run_sql(sql)
+        
+        return super().reset(seed)
+    
+    def close(self):
+        """ Close connection to database. """
+        self.connection.close()
+    
+    def _add_index(self):
+        """ Index the next data batch. """
+        if self.nr_indexed < nr_batches:
+            sql = (
+                f'create index if not exists shipdateindex{self.nr_indexed} ' 
+                f'on lineitem{self.nr_indexed}(l_shipdate);')
+            self._run_sql(sql)
+            self.nr_indexed += 1
+    
+    def _benchmark(self):
+        """ Benchmark execution time for a simple query.
+        
+        Returns:
+            Execution time for query in seconds.
+        """
+        sub_queries = []
+        for i in range(nr_batches):
+            sub_queries += [f'(select max(l_shipdate) from lineitem{i})']
+        sql = 'select greatest(' + ' ,'.join(sub_queries) + ');'
+        start_s = time.time()
+        self._run_sql(sql)
+        total_s = time.time() - start_s
+        print(f'Benchmark execution time: {total_s}')
+        return total_s
+    
+    def _drop_index(self):
+        """ Drop index for one data batch. """
+        if self.nr_indexed > 0:
+            sql = f'drop index if exists shipdateindex{self.nr_indexed}'
+            self._run_sql(sql)
+            self.nr_indexed -= 1
+    
+    def _reward(self):
+        """ Calculate reward by benchmarking simple query.
+        
+        Returns:
+            a reward value.
+        """
+        cur_secs = self._benchmark()
+        reward = (self.default_secs - cur_secs) / self.default_secs
+        reward = max(0, reward)
+        return reward
+    
+    def _run_sql(self, sql):
+        """ Run an SQL query on the database.
+        
+        Args:
+            sql: SQL query to execute.
+        """
+        print(f'About to execute query: {sql}')
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql)
--- src/tupac/tuner.py
+++ src/tupac/tuner.py
@@ -16,14 +16,14 @@ if __name__ == '__main__':
     parser.add_argument('password', type=str, help='Database password')
     args = parser.parse_args()
     
-    env = tupac.engine.PgSimEnv()
+    env = tupac.engine.PgEnv(args.db, args.user, args.password)
     model = stable_baselines3.A2C(
         'MlpPolicy', env, verbose=True, 
         normalize_advantage=True)
-    model.learn(total_timesteps=100)
+    model.learn(total_timesteps=1)
     
-    for step, nr_indexed in env.log:
-        print(f'{step}\t{nr_indexed}')
+    for step, nr_indexed, total_s, reward in env.log:
+        print(f'{step}\t{nr_indexed}\t{total_s}\t{reward}')
     
     # print('Testing Now!!!')
     # env = model.get_env()
